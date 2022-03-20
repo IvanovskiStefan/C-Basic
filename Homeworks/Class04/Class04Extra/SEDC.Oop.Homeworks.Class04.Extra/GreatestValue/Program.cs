7c1019800251d5ca86391e6ec62dfbe5d9486a17
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-20 12:53:41 +0100

    updating Class4/extraHomeworks

---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/GreatestValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreatestValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 356421 , 12, 15, 1, 886, 900, 11020 };
+            int biggestValue = values[0];
+
+            ReturnBiggest(values,biggestValue);
+        }
+        public static  void ReturnBiggest(int[] values, int biggestValue)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] > biggestValue)
+                {
+                    biggestValue = values[i];
+                }
+            }
+            Console.WriteLine(biggestValue); ;
+        }
+    
+    }
+}
---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/Palindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the word if you want to check if the word is a palindrome");
+
+            string inputFromScreen = Console.ReadLine();
+            //so malce pomos od youtube, se iznamaciv dodeka svatam deka treba da proveri karakter po karakter :D
+            char[] normal = inputFromScreen.ToCharArray();
+            char[] reversed = inputFromScreen.ToCharArray();
+            Array.Reverse(reversed);
+
+
+            TheWordFromInput( normal, reversed);
+        }
+
+        public static void TheWordFromInput(char[] normal, char[] reversed)
+        {
+            bool palindrome = true;
+           
+            for (int i = 0; i < normal.Length; i++)
+            {   //&& normal.ToString().ToLower() != reversed.ToString().ToLower() uste edna proverka zosto palindormot ako e napisan
+                //so prvata bukva golema booleanot izlagase false sekogas, a zborot e palindrom
+                 
+                if (normal[i] != reversed[i] && normal.ToString().ToLower() != reversed.ToString().ToLower())
+                {
+                   
+                    palindrome = false;
+                }
+                
+
+            }
+            Console.WriteLine($"Is the word a palindrome? : {palindrome}");
+
+        }
+    }
+}
+
+
