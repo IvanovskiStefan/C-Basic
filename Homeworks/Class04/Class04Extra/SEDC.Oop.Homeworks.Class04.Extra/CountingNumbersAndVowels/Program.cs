9e3f28b9e1b41a0dfbed07c4154f5857a86c54e3
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-20 14:34:23 +0100

    update on homework.class4/extra

---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/CountingNumbersAndVowels/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CountingNumbersAndVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the text that you want to find out how many characters has and how many vowels it contains");
+
+            string inputFromConsole = Console.ReadLine().ToLower();
+            countingChars(inputFromConsole);
+            countingVowels(inputFromConsole);
+        }
+        public static void countingChars(string inputFromConsole)
+        {
+            int count = inputFromConsole.Length;
+            Console.WriteLine($"The number of digits is : {count}");
+        }
+        public static void countingVowels(string inputFromConsole)
+        {
+            int totalVowels = 0;
+            foreach ( char vowel in inputFromConsole)
+            {
+                switch (vowel)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        totalVowels++;
+                        break;
+                    
+                }
+
+            }
+            Console.WriteLine("The total numbers of vowels is: {0}", totalVowels);
+        }
+            
+        }
+    }
+
