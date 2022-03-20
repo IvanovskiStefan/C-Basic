76949edef7f108a8638a10275d1e37fa73bc27a7
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-20 14:56:31 +0100

    update on Homework Class 04/Extra

---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/UpperToLowerViceVersa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UpperToLowerViceVersa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the sentence that you want to be converted," +
+                "from uppercase to lowercase and vice versa");
+            string inputFromConsole = Console.ReadLine();
+            char[] arrayFromConsole = inputFromConsole.ToCharArray();
+            conversion(inputFromConsole);
+        }
+
+        public static void conversion(string arrayFromConsole)
+        {
+            string convertedString = string.Empty;
+
+            for(int i = 0; i < arrayFromConsole.Length; i++)
+            {
+                if (char.IsUpper(arrayFromConsole[i]))
+                {
+                    convertedString += char.ToLower(arrayFromConsole[i]);
+                } else
+                    convertedString += char.ToUpper(arrayFromConsole[i]);
+
+            }
+            Console.WriteLine($"The converted sentence is : {convertedString}");
+
+        }
+        }
+    }
+
