1b46ffd4ffa70eb7810ab4ac734bc7e1ec746f57
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-20 15:27:09 +0100

    Update on Homework4/Extra

---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/WhatTypeIsTheChar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WhatTypeIsTheChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character to find out if the char is an alphabet");
+            string inputFromCons= Console.ReadLine();
+            char[] arrayFromCons = inputFromCons.ToCharArray();
+            isTheCharAlpha(inputFromCons);
+        }
+
+        public static void isTheCharAlpha( string arrayFromCons)
+        {
+            for(int i = 0; i < arrayFromCons.Length; i++)
+            {
+                bool isCharacterAlpha = true;
+                bool isCharacterDigit = true;
+
+                if (Char.IsLetter(arrayFromCons[i]) == isCharacterAlpha)
+                {
+                    Console.WriteLine($"The character is alphabet");
+                } 
+                
+                else if (Char.IsDigit(arrayFromCons[i]) == isCharacterDigit)
+                
+                {
+                    Console.WriteLine($"The character is digit");
+                }
+
+                Console.WriteLine("Enter a valid input");
+
+
+            }
+            
+        }
+    }
+}
