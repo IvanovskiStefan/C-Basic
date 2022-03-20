5758191a53eb510ea5bdfb020f0858edf29abd93
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-20 22:46:51 +0100

    Updating Extra from Class4

---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/RasingIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RasingIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Number to be raised(isparsirani)
+            Console.WriteLine("enter the number that you want to raise :D");
+            string numberToBeRaised = Console.ReadLine();
+            bool parsedNumToBeRaised = int.TryParse(numberToBeRaised, out int parsedNumber1);
+            //2.Number to raise the first Num!(isparsirani)
+            Console.WriteLine("enter how much do you want  to raise the first number :D");
+            string numberToRaiseFirst = Console.ReadLine();
+            bool parsedNumberToRaiseFirst = int.TryParse(numberToRaiseFirst, out int parsedNumber2);
+            NumbersToBeRaised(parsedNumber1, parsedNumber2);
+        }
+        public static void NumbersToBeRaised(int parsedNumber1, int parsedNumber2)
+        {
+            int counter = 1;
+            for (int i = 0; i < parsedNumber2; i++)
+            {
+                counter = counter * parsedNumber1;
+            }
+            Console.WriteLine( $"The number : {parsedNumber1}, raised by {parsedNumber2} is : {counter}"); 
+        }
+
+    }  
+    
+}
---
+++ Homeworks/Class04/Class04Extra/SEDC.Oop.Homeworks.Class04.Extra/Substring/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string inputFromCon = Console.ReadLine();
+            Console.WriteLine("enter the data that you want to see how many times it appears in the string");
+            string inputForCounting = Console.ReadLine();
+           
+            substringCount(inputFromCon, inputForCounting);
+        }
+        public static void substringCount(string inputFromCon, string inputForCounting)
+        {
+            int strt = 0;
+            int cnt = -1;
+            int idx = -1;
+            
+
+            while( strt != -1)
+            {
+                strt = inputFromCon.IndexOf(inputForCounting, idx + 1);
+                    cnt += 1;
+                    idx = strt;
+            }
+            Console.Write("The string '{0}' occurs " + cnt , inputForCounting);
+            // ovaa ne ja razbrav, copy paste e od w3schools, i ne ja smetam za resena !!! :D
+
+        }
+    }
+}
+
+
