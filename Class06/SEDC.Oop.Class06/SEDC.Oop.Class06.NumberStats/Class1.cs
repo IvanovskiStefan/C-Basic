4651c427718a57365358b9dcf1689ca266854d10
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-03-21 12:11:27 -0700

    Class6 exc

---
+++ Class06/SEDC.Oop.Class06/SEDC.Oop.Class06.NumberStats/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Class06.NumberStats
+{
+    class infoModel
+    {
+        public bool IsPositive { get; set; }
+        public bool IsOddM { get; set; }
+        public bool isDecimal { get; set; }
+
+        public double Num { get; set; }
+
+        public infoModel(double num)
+        {
+            Num = num;
+        }
+        public  void CalculateNumber()
+        {
+            IsPositive = Num >= 0;
+            IsOddM = Num % 2 == 0;
+            isDecimal = Num % 1 == 0;
+                        
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine(IsPositive ? "Positive" : "Negative");
+            Console.WriteLine(IsOddM ? "Even" : "Odd");
+            Console.WriteLine(isDecimal ? "Integer" : "Decimal");
+
+        }
+    }
+
+
+}
---
+++ Class06/SEDC.Oop.Class06/SEDC.Oop.Class06.NumberStats/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SEDC.Oop.Class06.NumberStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to check it's status :D");
+            string inputFromUser = Console.ReadLine();
+            bool parsedInput = double.TryParse(inputFromUser, out double ParsedInputFromUser);
+            infoModel model = new infoModel(ParsedInputFromUser);
+            model.CalculateNumber();
+            model.PrintStats();
+        }
+
+        
+    }
+
+   
+}
